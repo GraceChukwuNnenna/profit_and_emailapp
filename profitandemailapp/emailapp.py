bdbf91d8c0820ca2e0fda16e64837096c8ed0969
Grace <127057400+GraceChukwuNnenna@users.noreply.github.com>
2023-05-18 19:50:46 +0100

    Add files via upload

---
+++ profitandemailapp/emailapp.py
@@ -0,0 +1,42 @@
+#  Create a project that will send an automatic email every 10mins 
+
+import smtplib
+from email.message import EmailMessage
+import ssl
+import datetime
+import time
+
+timenow = datetime.datetime.now()
+# time.sleep(20)
+
+# Create send_mail function
+def send_email():
+    sender = 'emchadexglobal@gmail.com'
+    recipient = 'emanstonepro@gmail.com','emchadexglobal@gmail.com'
+    mypassword = 'ewhkaqtxojttbbub'  
+    subject = 'Timed email testing in python'
+
+   # CREATE THE OBJECT OF THE EMAIL CLASS ;
+    email_me = EmailMessage()
+
+    email_me['Subject'] = subject
+    email_me['From'] = sender
+    email_me['To'] = recipient
+    email_me.set_content('This is my first timed email test in python. It sends email every 10mins')
+
+    # OPENNING THE CONNECTION TO GMAIL SERVER AND SAVING IT IN A VARIABLE 'mailme'
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as mailme:
+        mailme.login(sender, mypassword)
+        mailme.sendmail(sender, recipient, email_me.as_string())
+        # mailme.send_message(email_me)
+    print('email sent!')
+
+    if timenow.second == 20:
+        return mailme
+
+# while loop for infinite excecution of send_mail at every time.sleep()
+while True:
+    send_email()
+    time.sleep(600)
+
+send_email()
---
+++ profitandemailapp/profitapp.py
@@ -0,0 +1,37 @@
+# Craete an application that will gives a 10% profit every 24hours
+
+import datetime
+import time
+
+print('Welcome to Cohort 2 Asset management consult plc')
+
+username = input('Enter your name: ')
+amount = input('Enter Investment Amount: ')
+
+time.sleep(1*30)
+
+timenow = datetime.datetime.now()
+td = datetime.timedelta(seconds=24 * 60 * 60)
+
+def percentage_increase():
+    user_amount = int(amount)
+    total = user_amount * 0.10 + user_amount
+    commission = user_amount * 0.10
+    print(time.strftime("%c"))
+    print('Your commssion is', commission)
+    print(username,'congratulations your Capital is:',total)
+
+    time.sleep(1*30)
+
+ # While loop for infinite execution at every time.sleep(time)
+    while True:
+        timenow = td
+        if timenow.seconds == 0 * 1 * 30:
+            commission_roll = total * 0.1
+            total = total * 1.1
+            print(time.strftime("%c"))
+            print('Your commssion after 24hrs is', int(commission_roll))
+            print(username,'congratulations your New Capital after 24hrs is:',int(total))
+        time.sleep(1*30)
+
+percentage_increase()
